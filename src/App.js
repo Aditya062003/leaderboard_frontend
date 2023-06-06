903da4529a4df88d3a9bbb2d430b88bc21fdd0d2
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-06-06 14:01:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,239 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Grid } from "@material-ui/core";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Navbar } from "./components/Navbar.js";
+import { CodeforcesTable } from "./components/CodeforcesTable.js";
+import { CodechefTable } from "./components/CodechefTable";
+import { GithubTable } from "./components/GithubTable";
+import Profile1 from "./components/Profile1.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { OpenlakeTable } from "./components/OpenlakeTable";
+import Login from "./components/Login";
+import HomePage from "./components/HomePage";
+import Register from "./components/Register";
+import { LeetcodeTable } from "./components/LeetcodeTable";
+import PrivateRoute from "./utils/PrivateRoute";
+import { AuthProvider } from "./Context/AuthContext";
+import GoToTop from "./components/GoToTop";
+import Footer from "./components/Footer"
+import LeetcodeRankings from "./components/LeetcodeRankings";
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          scrollbarColor: "#686868 #686868",
+          "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+            backgroundColor: "#424242",
+          },
+          "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
+            borderRadius: 20,
+            backgroundColor: "#636363",
+            minHeight: 15,
+          },
+          "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover":
+            {
+              backgroundColor: "#4F4F4F",
+            },
+          "&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
+            backgroundColor: "#686868",
+          },
+        },
+      },
+    },
+  },
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          scrollbarColor: "#C1C1C1 #C1C1C1",
+          "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+            backgroundColor: "#F1F1F1",
+          },
+          "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
+            borderRadius: 20,
+            backgroundColor: "#C1C1C1",
+            minHeight: 15,
+          },
+          "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover":
+            {
+              backgroundColor: "#B5B5B5",
+            },
+          "&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
+            backgroundColor: "#C1C1C1",
+          },
+        },
+      },
+    },
+  },
+});
+function App() {
+  const [codechefUsers, setCodechefUsers] = useState([]);
+  const [darkmode, setDarkmode] = useState(false);
+  const [codeforcesUsers, setCodeforcesUsers] = useState([]);
+  const [leetcodeUsers, setLeetcodeUsers] = useState([]);
+  const [openlakeContributor, setOpenlakeContributor] = useState([]);
+  const [githubUser, setGithubUser] = useState([]);
+  const [codeforcesfriends, setCodeforcesfriends] = useState([]);
+  const [cfshowfriends, setCfshowfriends] = useState(false);
+  const [ccshowfriends, setCCshowfriends] = useState(false);
+  const [codecheffriends, setCodecheffriends] = useState([]);
+  const [ltshowfriends, setLtshowfriends] = useState(false);
+  const [leetcodefriends, setLeetcodefriends] = useState([]);
+  const [githubfriends, setGithubfriends] = useState([]);
+  const [ghshowfriends, setGhshowfriends] = useState(false);
+  const [openlakefriends, setOpenlakefriends] = useState([]);
+  const [olshowfriends, setOlshowfriends] = useState(false);
+  const toggle = () => {
+    setDarkmode(!darkmode);
+    const g = localStorage.getItem("dark-mode");
+    if (g === "off") localStorage.setItem("dark-mode", "on");
+    else localStorage.setItem("dark-mode", "off");
+  };
+  useEffect(() => {
+    const dm = localStorage.getItem("dark-mode");
+    if (dm != null) {
+      if (dm === "on") setDarkmode(true);
+      else setDarkmode(false);
+    }
+  }, []);
+  useEffect(() => {
+    fetch("https://leaderboard-stswe61wi-aditya062003.vercel.app/codeforces/")
+      .then((res) => res.json())
+      .then((res) => {
+        setCodeforcesUsers(res);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("https://leaderboard-stswe61wi-aditya062003.vercel.app/codechef/")
+      .then((res) => res.json())
+      .then((res) => {
+        setCodechefUsers(res);
+      });
+  }, []);
+  useEffect(() => {
+    fetch("https://leaderboard-stswe61wi-aditya062003.vercel.app/leetcode/")
+      .then((res) => res.json())
+      .then((res) => {
+        setLeetcodeUsers(res);
+      });
+  }, []);
+  useEffect(() => {
+    fetch("https://leaderboard-stswe61wi-aditya062003.vercel.app/openlake/")
+      .then((res) => res.json())
+      .then((res) => {
+        setOpenlakeContributor(res);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("https://leaderboard-stswe61wi-aditya062003.vercel.app/github/")
+      .then((res) => res.json())
+      .then((res) => {
+        setGithubUser(res);
+      });
+  }, []);
+
+  return (
+    <ThemeProvider theme={darkmode ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <Router>
+        <AuthProvider>
+          <div className="App">
+            <Navbar darkmode={darkmode} toggle={toggle}/>
+            <Grid container>
+              <Grid item xs={6}>
+                <Switch>
+                  <Route exact path="/register">
+                    <Register darkmode={darkmode} />
+                  </Route>
+                  <Route exact path="/login">
+                    <Login darkmode={darkmode} />
+                  </Route>
+                  <PrivateRoute exact path="/">
+                    <HomePage />
+                  </PrivateRoute>
+
+                  <PrivateRoute exact path="/codeforces">
+                    <CodeforcesTable
+                      darkmode={darkmode}
+                      codeforcesfriends={codeforcesfriends}
+                      setCodeforcesfriends={setCodeforcesfriends}
+                      codeforcesUsers={codeforcesUsers}
+                      cfshowfriends={cfshowfriends}
+                      setCfshowfriends={setCfshowfriends}
+                    />
+                  </PrivateRoute>
+                  <PrivateRoute exact path="/codechef">
+                    <CodechefTable
+                      darkmode={darkmode}
+                      codechefUsers={codechefUsers}
+                      codecheffriends={codecheffriends}
+                      setCodecheffriends={setCodecheffriends}
+                      ccshowfriends={ccshowfriends}
+                      setCCshowfriends={setCCshowfriends}
+                    />
+                  </PrivateRoute>
+                  <PrivateRoute exact path="/openlake">
+                    <OpenlakeTable
+                      darkmode={darkmode}
+                      codechefUsers={openlakeContributor}
+                      codecheffriends={openlakefriends}
+                      setCodecheffriends={setOpenlakefriends}
+                      ccshowfriends={olshowfriends}
+                      setCCshowfriends={setOlshowfriends}
+                    />
+                  </PrivateRoute>
+                  <PrivateRoute exact path="/github">
+                    <GithubTable
+                      darkmode={darkmode}
+                      githubUsers={githubUser}
+                      githubfriends={githubfriends}
+                      setGithubfriends={setGithubfriends}
+                      ghshowfriends={ghshowfriends}
+                      setGHshowfriends={setGhshowfriends}
+                    />
+                  </PrivateRoute>
+                  <PrivateRoute exact path="/leetcode">
+                    <LeetcodeTable
+                      darkmode={darkmode}
+                      leetcodeUsers={leetcodeUsers}
+                      leetcodefriends={leetcodefriends}
+                      setLeetcodefriends={setLeetcodefriends}
+                      ltshowfriends={ltshowfriends}
+                      setLTshowfriends={setLtshowfriends}
+                    />
+                  </PrivateRoute>
+                  <PrivateRoute exact path="/profile">
+                    <Profile1 darkmode={darkmode} />
+                  </PrivateRoute>
+                  <PrivateRoute exact path="/leetcoderankings">
+                    <LeetcodeRankings darkmode={darkmode} />
+                  </PrivateRoute>
+                  <Route exact path="/*">
+                    <HomePage />
+                  </Route>
+                </Switch>
+              </Grid>
+            </Grid>
+            <GoToTop/>
+            <Footer/>
+          </div>
+        </AuthProvider>
+      </Router>
+    </ThemeProvider>
+  );
+}
+export default App;
---
+++ src/components/SignUp.js
@@ -0,0 +1,193 @@
+import {React,useContext} from 'react'
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AuthContext from '../Context/AuthContext.js';
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://github.com/OpenLake/Leaderboard-Pro/">
+        LeaderBoard-Pro
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignUp({registerUser}) {
+  let {toLogin}=useContext(AuthContext);
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={registerUser}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={1}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  size="small"
+                  name="first_name"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  size="small"
+                  id="last_name"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                  defaultValue=""
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  size="small"
+                  name="username"
+                  label="username"
+                  type="username"
+                  id="username"
+                  autoComplete="new-username"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                
+                  required
+                  fullWidth
+                  size="small"
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  type="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  size="small"
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  size="small"
+                  name="CodeChef Username"
+                  label="CodeChef Username"
+                  type="text"
+                  id="cc_uname"
+                  autoComplete="new-cc_uname"
+                  defaultValue=""
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  size="small"
+                  name="Codeforces Username"
+                  label="Codeforces Username"
+                  type="text"
+                  id="cf_uname"
+                  autoComplete="new-cf_uname"
+                  defaultValue=""
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  size="small"
+                  name="Github Username "
+                  label="Github Username  "
+                  type="text"
+                  id="gh_uname"
+                  autoComplete="new-gh_uname"
+                  defaultValue=""
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                size="small"
+                  fullWidth
+                  name="LeetCode Username "
+                  label="LeetCode Username  "
+                  type="text"
+                  id="lt_uname"
+                  autoComplete="new-lt_uname"
+                  defaultValue=""
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container alignItems="center" justifyContent="center">
+              <Grid item>
+                <Button  variant="body2" style={{color:"red"}} onClick={toLogin}>
+                  Already have an account? Sign in
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
