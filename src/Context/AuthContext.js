e9d281b87ff8d533e85f07293acf7004a102946c
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-06-10 17:48:40 +0530

    Update AuthContext.js

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -14,7 +14,7 @@ export const AuthProvider=({children})=>{
     let loginUser=async (e)=>
     {
         e.preventDefault();
-        let response=await fetch('https://leaderboard-frontend-qzqs4q3ti-aditya062003.vercel.app/leetcoderankingsccps/api/token/',{
+        let response=await fetch('https://leaderboard-frontend-qzqs4q3ti-aditya062003.vercel.app/api/token/',{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
@@ -44,7 +44,7 @@ export const AuthProvider=({children})=>{
     let registerUser=async(e)=>
     {
         e.preventDefault();
-        let response=await fetch('https://leaderboard-frontend-qzqs4q3ti-aditya062003.vercel.app/leetcoderankingsccps/api/register/',{
+        let response=await fetch('https://leaderboard-frontend-qzqs4q3ti-aditya062003.vercel.app/api/register/',{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
@@ -57,7 +57,7 @@ export const AuthProvider=({children})=>{
         })
         if(response.status===200)
         {
-            let response=await fetch('https://leaderboard-frontend-qzqs4q3ti-aditya062003.vercel.app/leetcoderankingsccps/api/token/',{
+            let response=await fetch('https://leaderboard-frontend-qzqs4q3ti-aditya062003.vercel.app/api/token/',{
                 method:'POST',
                 headers:{
                     'Content-Type':'application/json'
@@ -85,7 +85,7 @@ export const AuthProvider=({children})=>{
         e.preventDefault();
         console.log("!!!!!!!!!!!!!!!!!!!!!!!!!")
         // console.log(JSON.parse(localStorage.getItem('authTokens')).access);
-        let response=await fetch('https://leaderboard-frontend-qzqs4q3ti-aditya062003.vercel.app/leetcoderankingsccps/api/insertapi/',{
+        let response=await fetch('https://leaderboard-frontend-qzqs4q3ti-aditya062003.vercel.app/api/insertapi/',{
             method:'POST',
             headers:{
                 'Content-Type':'application/json',
